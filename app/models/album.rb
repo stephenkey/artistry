c3fffa877fdb461005e214113f150d3367ed4817
Stephen Key <stephenkey123@gmail.com>
2017-10-25 16:05:53 -0500

    Adding album model.

---
+++ app/models/album.rb
@@ -0,0 +1,29 @@
+class Album < ApplicationRecord
+  include AASM
+
+  belongs_to :artist, counter_cache: true
+  belongs_to :genre, counter_cache: true
+
+  has_many :tracks
+
+  validates :title, presence: true
+  validates :artist_id, presence: true
+
+  aasm column: 'state', whiny_transitions: false do
+    state :new, :initial => true
+    state :refurbished
+    state :like_new
+    state :good
+    state :acceptable
+    state :unacceptable
+
+    event :refurbish do
+      transitions from: [:like_new, :good, :acceptable, :unacceptable], to: :refurbished
+    end
+
+    event :break do
+      transitions from: [:new, :refurbished, :like_new, :good, :acceptable], to: :unacceptable
+    end
+  end
+
+end
